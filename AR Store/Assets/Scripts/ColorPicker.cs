de6d7fc05f70df13b2747cdd4b8d1e29346d5117
Dmitri <lipov758@gmail.com>
2019-04-16 01:25:37 +0300

    Add scene with chair

---
+++ AR Store/Assets/Scripts/ColorPicker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPicker : MonoBehaviour
+{
+    public Material[] bodyMaterial;
+    Material currentMaterial;
+    new Renderer render;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        render = this.GetComponent<Renderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RedColor()
+    {
+        render.material = bodyMaterial[1];
+        currentMaterial = render.material;
+    }
+    public void BlueColor()
+    {
+        render.material = bodyMaterial[2];
+        currentMaterial = render.material;
+    }
+
+    public void StandartColor()
+    {
+        render.material = bodyMaterial[0];
+        currentMaterial = render.material;
+    }
+}
---
+++ AR Store/Assets/Scripts/RotateObject.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    //float rotSpeed = 20;
+
+    //void OnMouseDrag()
+    //{
+    //    float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+    //    //float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+
+    //    transform.RotateAround(Vector3.up, -rotX);
+    //    //transform.RotateAround(Vector3.right, rotY);
+
+    //    Debug.Log("Rotate");
+    //}
+
+
+
+    #region ROTATE
+    private float _sensitivity = 0.5f;
+    private Vector3 _mouseReference;
+    private Vector3 _mouseOffset;
+    private Vector3 _rotation = Vector3.zero;
+    private bool _isRotating;
+
+
+    #endregion
+
+    void Update()
+    {
+        if (_isRotating)
+        {
+            // offset
+            _mouseOffset = (Input.mousePosition - _mouseReference);
+
+            // apply rotation
+            _rotation.y = -(_mouseOffset.x + _mouseOffset.y) * _sensitivity;
+
+            // rotate
+            gameObject.transform.Rotate(_rotation);
+
+            // store new mouse position
+            _mouseReference = Input.mousePosition;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        // rotating flag
+        _isRotating = true;
+
+        // store mouse position
+        _mouseReference = Input.mousePosition;
+    }
+
+    void OnMouseUp()
+    {
+        // rotating flag
+        _isRotating = false;
+    }
+
+}
+
