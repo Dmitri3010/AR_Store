c608f0be939cb83ec84bc9e3be08b58efca8a39e
Dmitri <lipov758@gmail.com>
2019-06-05 03:43:56 +0300

    Add single product page

--- AR Store/Assets/Scripts/LoadObjects.cs
+++ AR Store/Assets/Scripts/LoadObjects.cs
@@ -17,18 +17,23 @@ public class LoadObjects : MonoBehaviour
     private float rotateSpeed = 200f;
     public bool loading;
     public GameObject spiner;
+    public GameObject SingleProductCost, SingleProductTest, SingleProductImage;
+    public List<Product> listOfProducts;
+    public GameObject MainObj, SingleObj;
 
     // Start is called before the first frame update
     [Obsolete]
     IEnumerator Start()
     {
+        MainObj.active = true;
+        SingleObj.active = false;
         rectComponent = GameObject.Find("Loading Circle").GetComponent<RectTransform>();
         Hashtable headers = new Hashtable();
         headers.Add("Content-Type", "application/json");
         Debug.Log(headers.ToString());
         UnityWebRequest readingsite = UnityWebRequest.Get("http://localhost:5000/api/products");
         readingsite.SetRequestHeader("Content-Type", "application/json");
-        readingsite.method = "GET"; 
+        readingsite.method = "GET";
         yield return readingsite.Send();
         while (!readingsite.isDone)
             yield return null;
@@ -44,9 +49,10 @@ public class LoadObjects : MonoBehaviour
 
         ProductListObject products = Processjson(JsonDataString);
         Debug.Log(products.Products.Count);
-        
+
         foreach (var item in products.Products)
         {
+            
             WWW www = new WWW(item.model);
             while (!www.isDone)
                 yield return null;
@@ -55,6 +61,8 @@ public class LoadObjects : MonoBehaviour
             Texture2D textWebPic = www.texture;
             Debug.Log("ddd " + textWebPic.height);
             Sprite = Sprite.Create(textWebPic, new Rect(0, 0, textWebPic.width, textWebPic.height), new Vector2(0, 0));
+            item.sprite = Sprite;
+            listOfProducts.Add(item);
             Debug.Log(item.name);
             Generate(item.name, Sprite);
         }
@@ -62,7 +70,7 @@ public class LoadObjects : MonoBehaviour
         spiner.active = false;
     }
 
-   
+
 
     // Update is called once per frame
     void Update()
@@ -74,15 +82,38 @@ public class LoadObjects : MonoBehaviour
     {
         Debug.Log("deserialaze " + jsonString);
 
-        return JsonUtility.FromJson<ProductListObject>(jsonString);        
+        return JsonUtility.FromJson<ProductListObject>(jsonString);
     }
-   public void Generate(string name, Sprite image)
+    public void Generate(string name, Sprite image)
     {
         GameObject scrollItemObj = Instantiate(scrollItemPrefab);
         scrollItemObj.transform.SetParent(scrollContent.transform, false);
         scrollItemObj.transform.Find("Text").gameObject.GetComponent<Text>().text = name;
         scrollItemObj.transform.Find("Image").gameObject.GetComponent<Image>().sprite = image;
+        scrollItemObj.GetComponent<Button>().onClick.AddListener((delegate { OpenSingleProduct(scrollItemObj.transform.Find("Text").gameObject.GetComponent<Text>().text); }));
 
+    }
+
+    public void OpenSingleProduct(string name)
+    {
+        foreach (var prod in listOfProducts)
+        {
+            if (prod.name == name)
+            {
+                SingleProductTest.GetComponent<Text>().text = prod.description;
+                SingleProductCost.GetComponent<Text>().text = prod.cost;
+                SingleProductImage.GetComponent<Image>().sprite = prod.sprite;
+            }
+        }
+        MainObj.active = false;
+        SingleObj.active = true;
+
+    }
+
+    public void ReturnToMain()
+    {
+        MainObj.active = true;
+        SingleObj.active = false;
     }
 }
 
@@ -114,6 +145,8 @@ public class Product
     public List<string> texturesObj;
 
     public Texture2D image;
+
+    public Sprite sprite;
 
 }
 
